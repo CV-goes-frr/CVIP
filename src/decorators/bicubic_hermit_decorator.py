bb37fa9f688d2b0259271ac27025790cffa51fe4
Maksim Kotenkov <maximkosmos5360@gmail.com>
2023-10-20 22:02:09 +0700

    Comments in code

--- src/decorators/bicubic_hermit_decorator.py
+++ src/decorators/bicubic_hermit_decorator.py
@@ -14,6 +14,7 @@ def bicubic_hermit_cache(function):
     @lru_cache(maxsize=128)
     def cached_wrapper(hashable_array1: tuple, hashable_array2: tuple,
                        hashable_array3: tuple, hashable_array4: tuple, t: int):
+        # cast tuples to np.array(s)
         array1 = np.array(hashable_array1)
         array2 = np.array(hashable_array2)
         array3 = np.array(hashable_array3)
@@ -23,6 +24,7 @@ def bicubic_hermit_cache(function):
     @wraps(function)
     def wrapper(array1: np.ndarray, array2: np.ndarray,
                 array3: np.ndarray, array4: np.ndarray, t: int):
+        # cast np.ndarray(s) to tuples
         return cached_wrapper(tuple(array1), tuple(array2), tuple(array3), tuple(array4), t)
 
     # copy lru_cache attributes over too
