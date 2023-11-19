58895adc6754a5da2c53cdb005649419d38fd74d
Maksim Kotenkov <maximkosmos5360@gmail.com>
2023-11-19 13:31:41 +0700

    Mediapipe that doesn't work

---
+++ src/filters/OverlayingMask.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+from multiprocessing import Pool
+
+from src.filters.Filter import Filter
+from src.linal.RtcUmeyama import RtcUmeyama
+
+
+class Mask(Filter):
+    def __init__(self, mask_name: str):
+        self.mask_name = mask_name
+        super().__init__()  # Call the constructor of the parent class (Filter)
+
+    def apply(self, img: np.ndarray, processes_limit: int):
+
+        # Find points on the mask
+        mask_image = cv2.imread(self.mask_name)
+        mp_face_mesh = mp.solutions.face_mesh
+        face_points = mp_face_mesh.FaceMesh(
+            min_detection_confidence=0.5, min_tracking_confidence=0.5)
+        rgb_image = cv2.cvtColor(mask_image, cv2.COLOR_BGR2RGB)
+        mask_points = face_points.process(rgb_image)
+
+        for face_landmarks in mask_points.multi_face_landmarks:
+            landmarks = []
+            for landmark in face_landmarks.landmark:
+                x, y = int(landmark.x * img.shape[1]), int(landmark.y * img.shape[0])
+                landmarks.append((x, y))
+
+            mask_landmarks = np.array(landmarks)
+
+        # Find points on target faces
+        rgb_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        target_faces_points = face_points.process(rgb_image)
+        landmarks_all = list()
+
+        if target_faces_points.multi_face_landmarks:
+            for face_landmarks in target_faces_points.multi_face_landmarks:
+                landmarks = []
+                for landmark in face_landmarks.landmark:
+                    x, y = int(landmark.x * img.shape[1]), int(landmark.y * img.shape[0])
+                    landmarks.append((x, y))
+
+                points = np.array(landmarks)
+                landmarks_all.append(points)
+
+        print(landmarks_all)
+        for landmarks in landmarks_all:
+            R, t, c = RtcUmeyama(landmarks, mask_landmarks)
+            print(R, t, c)
+
+
+if __name__ == "__main__":
+    processor = Mask("face.jpg")
+    input_image = cv2.imread("arcane.png")
+    processor.apply(input_image, 2)
---
+++ src/linal/RtcUmeyama.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+
+def RtcUmeyama(A: np.ndarray, B: np.ndarray) -> tuple:
+    n, m = A.shape
+
+    centroid_A = np.mean(A, axis=0)
+    centroid_B = np.mean(B, axis=0)
+    variance = np.mean(np.linalg.norm(A - centroid_A, axis=1) ** 2)
+
+    H = (np.dot((A - centroid_A).T, (B - centroid_B))) / n
+    print("H: ", H)
+    print()
+
+    U, D, VT = np.linalg.svd(H)
+    d = np.sign(np.linalg.det(U) * np.linalg.det(VT))
+    S = np.diag([1] * (m - 1) + [d])
+
+    R = np.dot(U, np.dot(S, VT))
+    c = variance / np.trace(np.diag(D) @ S)
+    t = centroid_A - c * np.dot(R, centroid_B)
+
+    return R, t, c
+
+
+def main():
+    A = np.array([[23, 178],
+                  [66, 173],
+                  [88, 187],
+                  [119, 202],
+                  [122, 229],
+                  [170, 232],
+                  [179, 199]])
+    B = np.array([[232, 38],
+                  [208, 32],
+                  [181, 31],
+                  [155, 45],
+                  [142, 33],
+                  [121, 59],
+                  [139, 69]])
+
+    R, t, c = RtcUmeyama(A, B)
+    print("R: ", R)
+    print("t: ", t)
+    print("c: ", c)
+
+
+if __name__ == "__main__":
+    main()
