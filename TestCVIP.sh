0f2c2e01bdc299545530191f5ca84cdd57165405
Maksim Kotenkov <maximkosmos5360@gmail.com>
2023-12-08 13:32:53 +0700

    TestCVIP.sh place results in the directory

--- TestCVIP.sh
+++ TestCVIP.sh
@@ -1,8 +1,7 @@
-./CVIP [-i=face.jpg]scale_to_resolution:1000:1000[-o=scaled_to_factor_parallel] --parallel_processes=4
-./CVIP [-i=photo.jpg]crop:0:0:100:100[-o=cropped]
-./CVIP [-i=face.jpg]nn_scale_with_factor:2[-o=nn_scaled] --parallel_processes=4
-./CVIP [-i=face.jpg]bilinear_scale_with_factor:2[-o=bilinear_scaled] --parallel_processes=4
-./CVIP [-i=photo.jpg]face_detection[-o=detected]
-./CVIP [-i=photo.jpg]face_blur:3[-o=blurred]
-./CVIP [-i=photo.jpg]mask:face.jpg[-o=masked]
-
+mkdir -p test_results
+./CVIP [-i=face.jpg]scale_to_resolution:1000:1000[-o=test_results/scaled_to_factor_parallel] --parallel_processes=4
+./CVIP [-i=photo.jpg]crop:0:0:100:100[-o=test_results/cropped]
+./CVIP [-i=face.jpg]nn_scale_with_factor:2[-o=test_results/nn_scaled] --parallel_processes=4
+./CVIP [-i=face.jpg]bilinear_scale_with_factor:2[-o=test_results/bilinear_scaled] --parallel_processes=4
+./CVIP [-i=photo.jpg]face_blur:3[-o=test_results/blurred]
+./CVIP [-i=photo.jpg]mask:face.jpg[-o=test_results/masked]
