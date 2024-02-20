aff15f8b5711a8083a775e7ca24f342a5c5e388e
Maksim Kotenkov <maximkosmos5360@gmail.com>
2024-02-20 10:30:55 +0700

    Handling old exceptions

--- CVIP.py
+++ CVIP.py
@@ -3,8 +3,9 @@ import argparse
 
 import cv2
 
-from src.Parser import Parser
 from settings import prefix
+from src.exceptions import WrongDependency, WrongFiltername, WrongParameters
+from src.Parser import Parser
 
 
 def main():
@@ -28,22 +29,32 @@ def main():
         with open('help.txt', 'r') as help_file:
             print(help_file.read())
     else:
-        process_lim = 1
-        if args.parallel_processes:
-            process_lim = args.parallel_processes
-        pars = Parser(args.actions, process_lim)
-        proc = pars.parse()
+        try:
+            process_lim = 1
+            if args.parallel_processes:
+                process_lim = args.parallel_processes
+            pars = Parser(args.actions, process_lim)
+            proc = pars.parse()
 
-        start: float = time.time()
-        print("\nPROCESSING...\n")
-        for fin in proc.fin_labels:
-            res_images_list = proc.process(fin)
-            cv2.imwrite(f'{prefix}/{fin}.jpg', res_images_list[0])
-            # create as many out files for every final label as we want
-            # for res_img_index in range(len(res_images_list)):
-            #     cv2.imwrite(f'{prefix}/{fin}{res_img_index}.jpg', res_images_list[res_img_index])
-        end: float = time.time()
-        print(f"\nALL TASKS WERE COMPLETED\nTIME ELAPSED: {end - start}\n")
+            start: float = time.time()
+            print("\nPROCESSING...\n")
+            for fin in proc.fin_labels:
+                res_images_list = proc.process(fin)
+                cv2.imwrite(f'{prefix}/{fin}.jpg', res_images_list[0])
+                # create as many out files for every final label as we want
+                # for res_img_index in range(len(res_images_list)):
+                #     cv2.imwrite(f'{prefix}/{fin}{res_img_index}.jpg', res_images_list[res_img_index])
+            end: float = time.time()
+            print(f"\nALL TASKS WERE COMPLETED\nTIME ELAPSED: {end - start}\n")
+        except WrongDependency as err:
+            print("Unknown dependency:", err)
+        except WrongFiltername as err:
+            print("Call of the unknown operation:", err)
+        except WrongParameters as err:
+            print("Wrong set of parameters for the operation:", err)
+        except Exception as err:
+            print(f"Unexpected {err=}, {type(err)=}")
+            raise
 
         
 if __name__ == "__main__":
