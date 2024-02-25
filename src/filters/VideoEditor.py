d7ac297de6cad06b8be99017b7c30544b41ed7c6
Maksim Kotenkov <maximkosmos5360@gmail.com>
2024-02-25 13:01:54 +0700

    VideoEditor integrated probably for every filter... but there may be problems with scaling and changing frames sizes

--- src/filters/VideoEditor.py
+++ src/filters/VideoEditor.py
@@ -5,31 +5,33 @@ import numpy as np
 from multiprocessing import Pool
 
 from .FaceDetection import FaceDetection
-
 from .Filter import Filter
+
 
 class VideoEditor(Filter):
 
-    def __init__ (self, filter_name: str):
+    def __init__(self, filter_name: str):
         super().__init__()
-        self.class_map: Dict[str, type] = {
-                                           "face_detection": FaceDetection} # Список применяемых фильтров
+        # self.class_map: Dict[str, type] = {
+        #                                    "face_detection": FaceDetection}  # Список применяемых фильтров
         # Этот список, конечно, можно расширить
-        self.filter = self.class_map[filter_name]
+        # self.filter = self.class_map[filter_name]
 
-    def apply(self, frames: np.ndarray, processes_limit: int, pool: Pool, num_frames: int, width: int, height: int):
+    @staticmethod
+    def apply(frames: np.ndarray, processes_limit: int, pool: Pool, filter: type, num_frames: int, width: int, height: int):
         print("Start VideoEditor")
-        output = np.empty((num_frames, height, width, 3), np.uint8) # Создаем список кадров
+        output = np.empty((num_frames, height, width, 3), np.uint8)  # Создаем список кадров
         index = 0
         for frame in frames:
-            frame = self.filter.apply(self, frame, processes_limit, pool) # Приемняем фильтр к каждому кадру
-            output[index, :, :, :] = frame[0] # Обязательно [0]
+            frame = filter.apply(frame, processes_limit, pool)  # Приемняем фильтр к каждому кадру
+            output[index, :, :, :] = frame[0]  # cz we return [img] in filters
             index += 1
-        return output
+        return [output]
+
 
 if __name__ == "__main__":
     processor = VideoEditor("face_detection")
-    cap = cv2.VideoCapture("Patrick.mp4") # Читаем изначальное видео
+    cap = cv2.VideoCapture("Patrick.mp4")  # Читаем изначальное видео
 
     # Находим характеристики изначального видео
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -48,17 +50,17 @@ if __name__ == "__main__":
     index = 0
 
     while True:
-        ret, frame = cap.read() # читаем каждый кадр видео
+        ret, frame = cap.read()  # читаем каждый кадр видео
         if not ret:
             break
         else:
-            frames[index, :, :, :] = frame # сохраняем каждый кадр видео
+            frames[index, :, :, :] = frame  # сохраняем каждый кадр видео
             index += 1
 
-    new_frames = processor.apply(frames, 2, Pool, num_frames, width, height) # Получаем кадры после применения фильтра
+    new_frames = processor.apply(frames, 2, Pool, num_frames, width, height)  # Получаем кадры после применения фильтра
 
     for value in new_frames:
-        out.write(value) # Записываем видео по кадрам в cv2.VideoWriter
+        out.write(value)  # Записываем видео по кадрам в cv2.VideoWriter
 
-    out.release() # Обязательно нужно закрыть
-    cap.release() # Иначе Иртегов пизды даст
+    out.release()  # Обязательно нужно закрыть
+    cap.release()  # Иначе Иртегов пизды даст
