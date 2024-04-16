7901f31050e684286dccb5022fbbeeb3d8c264bc
melarozz <melarozz18@gmail.com>
2024-04-16 23:32:31 +0700

    add: user choice of x,y offset of overlaying video add: user choice of resizing factor for overlaying video

--- src/stash/VideoOverlay.py
+++ src/stash/VideoOverlay.py
@@ -1,7 +1,7 @@
 import cv2
+import numpy as np
 
-
-def overlay_videos(video_path_1, video_path_2, output_path):
+def overlay_videos(video_path_1, video_path_2, output_path, resize_factor=4, x_offset=10, y_offset=10):
     cap1 = cv2.VideoCapture(video_path_1)
     cap2 = cv2.VideoCapture(video_path_2)
 
@@ -13,26 +13,36 @@ def overlay_videos(video_path_1, video_path_2, output_path):
     height2 = int(cap2.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps2 = cap2.get(cv2.CAP_PROP_FPS)
 
+    total_frames = min(int(cap1.get(cv2.CAP_PROP_FRAME_COUNT)), int(cap2.get(cv2.CAP_PROP_FRAME_COUNT)))
+
     fps = min(fps1, fps2)
 
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter(output_path, fourcc, fps, (width1, height1))
 
-    while True:
+    for _ in range(total_frames):
         ret1, frame1 = cap1.read()
         ret2, frame2 = cap2.read()
 
         if not ret1 or not ret2:
             break
 
-        frame2_resized = cv2.resize(frame2, (width2 // 4, height2 // 4))
+        frame2_resized = cv2.resize(frame2, (width2 // resize_factor, height2 // resize_factor))
 
-        x_offset = 10
-        y_offset = height1 - frame2_resized.shape[0] - 10
         frame1[y_offset:y_offset + frame2_resized.shape[0],
         x_offset:x_offset + frame2_resized.shape[1]] = frame2_resized
 
         out.write(frame1)
+
+    # If one video is longer, continue processing frames until both videos are exhausted
+    while ret1 or ret2:
+        if ret1:
+            ret1, frame1 = cap1.read()
+            out.write(frame1)
+        if ret2:
+            ret2, frame2 = cap2.read()
+            frame2 = np.zeros((height2, width2, 3), dtype=np.uint8)  # Black screen
+            out.write(frame1)
 
     cap1.release()
     cap2.release()
@@ -40,9 +50,12 @@ def overlay_videos(video_path_1, video_path_2, output_path):
 
     print("Overlay completed successfully!")
 
-
 video_path_1 = "./media/jerry1.mp4"
 video_path_2 = "./media/minion.mp4"
 output_path = "output_video.mp4"
 
-overlay_videos(video_path_1, video_path_2, output_path)
+resize_factor = int(input("Enter the resize factor for the second video: "))
+x_offset = int(input("Enter the x coordinate for the left upper corner of the second video: "))
+y_offset = int(input("Enter the y coordinate for the left upper corner of the second video: "))
+
+overlay_videos(video_path_1, video_path_2, output_path, resize_factor, x_offset, y_offset)
