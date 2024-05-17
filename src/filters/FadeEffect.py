a3923d9e1c54cb7fa235c6e9c0f9029c1296b534
melarozz <melarozz18@gmail.com>
2024-05-17 15:29:50 +0700

    add: CONTRIBUTING.md add: comments to new filters update: added new filters to README.md update: added new filters to help.txt

--- src/filters/FadeEffect.py
+++ src/filters/FadeEffect.py
@@ -7,12 +7,34 @@ from .Filter import Filter
 
 class FadeEffect(Filter):
     def __init__(self, fade_in_length: str, fade_out_length: str):
+        """
+        Initializes the FaceDetection filter.
+
+        Args:
+            fade_in_length (str): amount of frames that should be faded in the start of the video.
+            fade_out_length (str): amount of frames that should be faded out the end of the video.
+
+        Returns:
+            fade_in_length (int): amount of frames that should be faded in the start of the video.
+            fade_out_length (int): amount of frames that should be faded out the end of the video.
+        """
         super().__init__()
         self.log = "APPLYING FADE FILTER TO VIDEO..."
         self.fade_in_length = int(fade_in_length)
         self.fade_out_length = int(fade_out_length)
 
     def apply(self, frames: np.ndarray, processes_limit: int, pool: Pool) -> np.ndarray:
+        """
+        Applies feature matching to the input images.
+
+        Args:
+            frames (np.ndarray): NumPy array of frames.
+            processes_limit (int): Number of processes to use.
+            pool (Pool): Pool of processes.
+
+        Returns:
+            List[np.ndarray]: List containing the edited video frames as a NumPy array.
+        """
         num_frames = frames.shape[0]
 
         # Ensure fade lengths do not exceed the number of frames
--- src/filters/VideoToPanorama.py
+++ src/filters/VideoToPanorama.py
@@ -8,11 +8,29 @@ from .Filter import Filter
 
 class VideoToPanorama(Filter):
     def __init__(self):
+        """
+        Initializes the VideoToPanorama filter.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         super().__init__()
         self.log = "CREATING PANORAMA FROM VIDEO IN PROCESS..."
 
     def apply(self, frames: np.ndarray, processes_limit: int, pool: Pool) -> np.ndarray:
-        # print(frames.shape)
+        """
+        Converts video to panorama.
+
+        Args:
+            frames (np.ndarray): NumPy array of frames.
+            processes_limit (int): Number of processes to use.
+            pool (Pool): Pool of processes.
+        Returns:
+            np.ndarray: Array containing the panorama frame.
+        """
         step = 30
         result = frames[0]
 
